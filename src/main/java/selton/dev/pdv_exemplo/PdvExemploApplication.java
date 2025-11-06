5313461a5dd5c5a1f4fabb06f4826613e079b708
Selton Augusto <selthive@gmail.com>
2025-11-06 13:04:44 -0300

    create project, structure and database configutation

---
+++ src/main/java/selton/dev/pdv_exemplo/PdvExemploApplication.java
@@ -0,0 +1,13 @@
+package selton.dev.pdv_exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/selton/dev/pdv_exemplo/PdvExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package selton.dev.pdv_exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
